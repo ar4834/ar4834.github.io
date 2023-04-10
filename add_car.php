d139703fe039b0487063bf7bc774b2c6f2d03af8
ar4834 <130319480+ar4834@users.noreply.github.com>
2023-04-09 21:13:09 -0400

    Create add_car.php

---
+++ add_car.php
@@ -0,0 +1,20 @@
+<?php
+include "db_config.php";
+
+$make = $_POST['make'];
+$model = $_POST['model'];
+$year = $_POST['year'];
+$color = $_POST['color'];
+$plateNumber = $_POST['plateNumber'];
+$availability = isset($_POST['availability']) ? 1 : 0;
+
+$sql = "INSERT INTO Car (Make, Model, Year, Color, PlateNumber, Availability) VALUES ('$make', '$model', $year, '$color', '$plateNumber', $availability)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New car added successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
